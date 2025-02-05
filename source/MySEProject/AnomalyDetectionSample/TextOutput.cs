b5080792c93848b29914877d7769b84f12b22d49
sohelranalive <sohel.cse37@gmail.com>
2025-02-05 22:33:35 +0100

    removed unncesarry files

--- source/MySEProject/AnomalyDetectionSample/TextOutput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AnomalyDetectionSample
-{
-    public static class TextOutput
-    {
-        public static double TrainingTimeInSeconds { get; set; }
-        public static string OutputPath { get; set; }
-        public static double totalAvgAccuracy { get; set; }
-    }
-}
