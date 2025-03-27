e0bb95afab538cc20b40743b40a70dfe155e92cf
Ashiq <ashiq.cse102@gmail.com>
2025-03-27 23:39:54 +0100

    worked on presentation

--- source/MySEProject/AnomalyDetectionSample/OutputText.cs
+++ source/MySEProject/AnomalyDetectionSample/OutputText.cs
@@ -8,6 +8,7 @@ namespace AnomalyDetectionSample
 {
     public static class OutputText
     {
+        
         //Not explored yet
         //public static double TrainingTimeInSeconds { get; set; }
         //Not explored yet
